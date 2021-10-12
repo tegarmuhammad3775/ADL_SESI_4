f8d7fec4121f276c0ce3cba8db9f19d63f23b1e3
tegarmuhammad3775 <tegarmuhammad3775@gmail.com>
2021-10-12 17:52:47 +0700

    Ujian Days 4 (12 Oktober 2021)

--- ADL_Bootcamp_Days_4/src/Array_Package/BelajarArray.java
+++ ADL_Bootcamp_Days_4/src/Array_Package/BelajarArray.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class BelajarArray {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		BelajarArray bel = new BelajarArray();
 		bel.arrGame();
